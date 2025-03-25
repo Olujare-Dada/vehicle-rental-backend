603b210eb4fad4f7fdb14663730556562c1b2f68
Olujare <olanrewaju.dada@yahoo.com>
2025-03-25 05:37:04 -0400

    Add initial configuration files

---
+++ src/test/java/com/bptn/vehicle_project/VehicleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bptn.vehicle_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
